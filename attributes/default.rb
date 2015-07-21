ec339eb9c755dcc199ff64424db111a280ae2db3
Matthew Green <thematthewgreen@gmail.com>
2015-07-21 17:05:56 -0400

    pretty straight forward codes...

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+# encoding: UTF-8
+
+default['chromedriver']['version'] = '2.12'
+default['chromedriver']['platform'] = 'linux64'
+default['chromedriver']['archive'] = "chromedriver_#{node['chromedriver']['platform']}.zip"
+default['chromedriver']['source_url'] = "http://chromedriver.storage.googleapis.com/#{node['chromedriver']['version']}/#{node['chromedriver']['archive']}"
+default['chromedriver']['download_path'] = "#{Chef::Config[:file_cache_path]}/#{node['chromedriver']['archive']}"
+default['chromedriver']['install_path'] = '/usr/bin'
